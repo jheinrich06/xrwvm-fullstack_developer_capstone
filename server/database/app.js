ab7f62c5eb58127f7467d3445406899554a16b22
jheinrich06 <36543180+jheinrich06@users.noreply.github.com>
2024-11-02 21:23:05 -0500

    Update app.js

--- server/database/app.js
+++ server/database/app.js
@@ -92,7 +92,7 @@ app.get('/fetchDealer/:id', async (req, res) => {
 app.post('/insert_review', express.raw({ type: '*/*' }), async (req, res) => {
   data = JSON.parse(req.body);
   const documents = await Reviews.find().sort( { id: -1 } );
-  let new_id = documents[0]['id']+1
+  let new_id = documents[0].id+1;
 
   const review = new Reviews({
 		"id": new_id,
@@ -100,7 +100,7 @@ app.post('/insert_review', express.raw({ type: '*/*' }), async (req, res) => {
 		"dealership": data.dealership,
 		"review": data.review,
 		"purchase": data.purchase,
-		"purchase_date": data.purchase_date.,
+		"purchase_date": data.purchase_date,
 		"car_make": data.car_make,
 		"car_model": data.car_model,
 		"car_year": data.car_year,
