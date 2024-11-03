795f94ddad3819d0517db438a819e0210445cd8a
jheinrich06 <36543180+jheinrich06@users.noreply.github.com>
2024-11-02 21:38:16 -0500

    Update populate.py

--- server/djangoapp/populate.py
+++ server/djangoapp/populate.py
@@ -5,15 +5,15 @@ def initiate():
     car_make_data = [
         {"name": "NISSAN", "description": "Great cars. Japanese technology"},
         {"name": "Mercedes", "description": "Great cars. German technology"},
-        {"name":" Audi", "description": "Great cars. German technology"},
+        {"name": "Audi", "description": "Great cars. German technology"},
         {"name": "Kia", "description": "Great cars. Korean technology"},
         {"name": "Toyota", "description": "Great cars. Japanese technology"},
     ]
 
     car_make_instances = []
     for data in car_make_data:
-        car_make_instances.append(CarMake.objects.create(name=data['name'], \
-        description=data['description']))
+        car_make_instances.append(CarMake.objects.create(name=data['name'],
+            description=data['description']))
 
     # Create CarModel instances with the corresponding CarMake instances
     car_model_data = [
@@ -50,5 +50,5 @@ def initiate():
     ]
 
     for data in car_model_data:
-        CarModel.objects.create(name=data['name'], car_make=data['car_make'], \
+        CarModel.objects.create(name=data['name'], car_make=data['car_make'],
                                 type=data['type'], year=data['year'])
